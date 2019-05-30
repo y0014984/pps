9d089e7aca6d2741533358c20b0df2b7559d5b7f
y0014984 <50139270+y0014984@users.noreply.github.com>
2019-05-30 12:11:33 +0200

    solved issue #1
    Removing base class definitions for dialog and referencing build-in classes.

--- addons/pps_main/dialogs.hpp
+++ addons/pps_main/dialogs.hpp
@@ -5,6 +5,11 @@ STRG + I für den Import aus dem Config File:
 
 */
 
+class RscListbox;
+class RscText;
+class RscEdit;
+class RscButton;
+
 class PPS_Main_Dialog
 {
 	idd = 14984;
@@ -16,7 +21,7 @@ class PPS_Main_Dialog
 		// GUI EDITOR OUTPUT START (by y0014984|Sebastian, v1.063, #Wytyno)
 		////////////////////////////////////////////////////////
 
-		class PPS_RscText_1000: PPS_RscText
+		class PPS_RscText_1000: RscText
 		{
 			idc = 1000;
 			text = "Persistent Player Statistics - Event in Progress: Unknown"; //--- ToDo: Localize;
@@ -26,7 +31,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0.5,0,1};
 		};
-		class PPS_RscText_1001: PPS_RscText
+		class PPS_RscText_1001: RscText
 		{
 			idc = 1001;
 			text = "Server Status: Unknown - Database Status: Unknown"; //--- ToDo: Localize;
@@ -36,7 +41,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0,0,0.25};
 		};
-		class PPS_RscText_1002: PPS_RscText
+		class PPS_RscText_1002: RscText
 		{
 			idc = 1002;
 			text = "Players Total: Unknown - Players Online: Unknown - Admins Total: Unknown - Admins Online: Unknown"; //--- ToDo: Localize;
@@ -46,7 +51,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0,0,0.25};
 		};
-		class PPS_RscEdit_1400: PPS_RscEdit
+		class PPS_RscEdit_1400: RscEdit
 		{
 			idc = 1400;
 			text = ""; //--- ToDo: Localize;
@@ -56,7 +61,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0.5,0,0.5};
 		};
-		class PPS_RscEdit_1401: PPS_RscEdit
+		class PPS_RscEdit_1401: RscEdit
 		{
 			idc = 1401;
 			text = ""; //--- ToDo: Localize;
@@ -66,7 +71,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0.5,0,0.5};
 		};
-		class PPS_RscListbox_1500: PPS_RscListbox
+		class PPS_RscListbox_1500: RscListbox
 		{
 			idc = 1500;
 			x = 0 * GUI_GRID_W + GUI_GRID_X;
@@ -74,7 +79,7 @@ class PPS_Main_Dialog
 			w = 14.5 * GUI_GRID_W;
 			h = 17.5 * GUI_GRID_H;
 		};
-		class PPS_RscListbox_1501: PPS_RscListbox
+		class PPS_RscListbox_1501: RscListbox
 		{
 			idc = 1501;
 			x = 15 * GUI_GRID_W + GUI_GRID_X;
@@ -82,7 +87,7 @@ class PPS_Main_Dialog
 			w = 25 * GUI_GRID_W;
 			h = 17.5 * GUI_GRID_H;
 		};
-		class PPS_RscButton_1600: PPS_RscButton
+		class PPS_RscButton_1600: RscButton
 		{
 			idc = 1600;
 			text = "Admin"; //--- ToDo: Localize;
@@ -93,7 +98,7 @@ class PPS_Main_Dialog
 			
 			action = "[] call PPS_fnc_dialogButtonAdminExec;";
 		};
-		class PPS_RscButton_1602: PPS_RscButton
+		class PPS_RscButton_1602: RscButton
 		{
 			idc = 1602;
 			text = "Event"; //--- ToDo: Localize;
@@ -105,7 +110,7 @@ class PPS_Main_Dialog
 			action = "[] call PPS_fnc_dialogButtonEventExec;";
 		};
 		
-		class PPS_RscEdit_1603: PPS_RscEdit
+		class PPS_RscEdit_1603: RscEdit
 		{
 			idc = 1603;
 			text = ""; //--- ToDo: Localize;
@@ -115,7 +120,7 @@ class PPS_Main_Dialog
 			h = 1 * GUI_GRID_H;
 			colorBackground[] = {0,0.5,0,0.5};
 		};
-		class PPS_RscButton_1604: PPS_RscButton
+		class PPS_RscButton_1604: RscButton
 		{
 			idc = 1604;
 			text = "Track Statistics"; //--- ToDo: Localize;
@@ -126,7 +131,7 @@ class PPS_Main_Dialog
 			
 			action = "[] call PPS_fnc_dialogButtonTrackStatisticsExec;";
 		};
-		class PPS_RscButton_1601: PPS_RscButton
+		class PPS_RscButton_1601: RscButton
 		{
 			idc = 1601;
 			text = "Update"; //--- ToDo: Localize;
